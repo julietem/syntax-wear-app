be1d21b6dde92e614295ceeadfbe26b25ed30ca0
juliete Mendonça <jullimb10@gmail.com>
2026-02-26 16:02:58 -0300

    feat: adicionando componentes router

---
+++ src/pages/_app/layout.tsx
@@ -0,0 +1,18 @@
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+
+
+export const Route = createFileRoute("/_app")({
+  component: AppLayout,
+});
+
+function AppLayout() {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer/>
+        </div>
+    )
+}
